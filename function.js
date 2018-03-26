f455f1237db6f356a3ce6c19029a67e953279278
GomesMunchen <al.nique@hotmail.com>
2018-03-26 15:42:44 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+
+function progress (){
+     
+     var prg = document.getElementById('progress');
+     var percent = document.getElementById('percentCount');
+     var counter = 5;
+     var progress = 25;
+     var id = setInterval (frame, 50);
+     
+     function frame(){
+         if(progress == 500 && counter == 100){
+             clearInterval(id)
+             {    
+                  
+ $('#percentCount').html(function(){   
+          
+  swal("Parabéns!!! Você conseguiu uma vaga, estamos te redirecionando...")
+       
+.then((value) => {
+  swal(`Você tem 15 minutos para finalizar a operação.`);
+}) ;
+  });
+         }
+         }else {
+             progress += 5;
+             counter += 1;
+             prg.style.width = progress + 'px';
+             percent.innerHTML = counter + '%';
+         }
+         
+     }
+ }
+progress();
+
---
+++ index.php
@@ -0,0 +1,48 @@
+
+
+
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    <title>adireto- informando, motivando e transformando o mundo</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css"/>
+    
+    </head>
+<body>
+    
+        <div class="container percent-count">
+            <div class="row">
+                <div class="col-xs-4 col-xs-offset-4">
+                <img src="imagem/adireto_logo.png" class="logo center-block">
+                </div>
+            </div>
+    </div>
+    
+
+    <h1>Verificando vagas disponíveis</h1>
+    <div id="percentCount"class= "percent-count">   aguarde...</div>
+    
+    <div class="progress-bar">
+<div class="progress" id="progress">      
+        </div>
+    </div>
+<script src="function.js"></script>
+    
+    
+
+    <script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js "></script>
+    <script src = "https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    
+        
+    
+    </body>
+
+
+
+
+
+
+</html>
